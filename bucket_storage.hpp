e9810c68ac222a2f8aa3e37ecc35ac8a8d0e3f95
Eyeve <astralolia@yandex.ru>
2024-09-09 01:58:59 +0300

    BucketContent added + large implementations

--- bucket_storage.hpp
+++ bucket_storage.hpp
@@ -5,10 +5,14 @@
 #include <iterator>
 #include <limits>
 #include <utility>
+#include <cstdint>
+
+#define DEFAULT_BLOCK_CAPACITY 64
 
 // ------------------------------------------
 // START OF BUCKET STORAGE INTERFACE
 // ------------------------------------------
+
 template<typename T>
 class BucketStorage
 {
@@ -19,6 +23,7 @@ class BucketStorage
 	friend class AbstractIterator;
 
 	class Bucket;
+	class GeneralBucketContent;
 
   public:
 	using value_type = T;
@@ -28,7 +33,16 @@ class BucketStorage
 	using const_iterator = AbstractIterator< true >;
 	using difference_type = std::ptrdiff_t;
 	using size_type = std::size_t;
+	using id_type = uint64_t;
 
+  private:
+	GeneralBucketContent generalContent;
+	size_type dataSize;
+	Bucket* first;
+	Bucket* last;
+	Bucket* incomplete;
+
+  public:
 	/*
      * @brief Creates an empty container
 	 */
@@ -128,20 +142,95 @@ class BucketStorage
 };
 
 // ------------------------------------------
+// START OF GENERAL BUCKET CONTENT INTERFACE
+// ------------------------------------------
+
+template< typename T >
+class BucketStorage<T>::GeneralBucketContent {
+	const size_type blockCapacity;
+	id_type idCounter;
+
+  public:
+	GeneralBucketContent(size_type blockCapacity = DEFAULT_BLOCK_CAPACITY);
+	GeneralBucketContent(const BucketStorage<T>::GeneralBucketContent& other);
+
+	size_type getBlockCapacity() const noexcept;
+	id_type id() noexcept;
+};
+
+// ------------------------------------------
 // START OF BUCKET INTERFACE
 // ------------------------------------------
 
 template< typename T >
 class BucketStorage< T >::Bucket
 {
+	constexpr static size_type nullIndex = std::numeric_limits<size_type>::max();
+
+  public:
 	using size_type = size_t;
+	using reference = T&;
+	using pointer = T*;
+	using const_reference = const T&;
+	using const_pointer = const T*;
+
+  private:
+	GeneralBucketContent* const generalContent;
+	const id_type id;
+	Bucket* next;
+	Bucket* prev;
+	Bucket* nextIncomplete;
+	Bucket* prevIncomplete;
+	T* data;
+	size_type size;
+	size_type firstIndex;
+	size_type lastIndex;
+	size_type* nextData;
+	size_type* prevData;
+	id_type* idData;
+
+  public:
+	Bucket();
+	Bucket(GeneralBucketContent* generalContent, Bucket* last);
+	Bucket(GeneralBucketContent* generalContent, Bucket* prev, Bucket* last);
+	~Bucket();
+
+	void setNext(Bucket* value) { next = value; }
+	void setPrev(Bucket* value) { prev = value; }
+	void setNextIncomplete(Bucket* value) { nextIncomplete = value; }
+	void setPrevIncomplete(Bucket* value) { prevIncomplete = value; }
+	Bucket* getNext() const { return next; }
+	Bucket* getPrev() const { return prev; }
+	Bucket* getNextIncomplete() const { return nextIncomplete; }
+	Bucket* getPrevIncomplete() const { return prevIncomplete; }
+	[[nodiscard]] size_type beginIndex() const;
+	[[nodiscard]] size_type endIndex() const;
+	[[nodiscard]] size_type nextIndex(size_type index) const;
+	[[nodiscard]] size_type prevIndex(size_type index) const;
+	[[nodiscard]] Bucket* nextBucket() const;
+	[[nodiscard]] Bucket* prevBucket() const;
+	[[nodiscard]] bool isEnd() const;
+	reference getReference(size_type index);
+	pointer getPointer(size_type index);
+	const_reference getReference(size_type index) const;
+	const_pointer getPointer(size_type index) const;
+	iterator insert(const T& value);
+	iterator insert(T&& value);
+	[[nodiscard]] bool isFull() const;
+	[[nodiscard]] bool isEmpty() const;
+
+  private:
+	void checkFreeSpace() const;
+	size_type prepareInsert();
+	template< typename U >
+	[[nodiscard]] U* getMemory(size_type count) const;
 };
 
 // ------------------------------------------
 // START OF ITERATOR INTERFACE
 // ------------------------------------------
 
-template< class T >
+template< typename T >
 template< bool isConst >
 class BucketStorage< T >::AbstractIterator
 {
@@ -152,13 +241,18 @@ class BucketStorage< T >::AbstractIterator
 	using difference_type = std::ptrdiff_t;
 	using iterator_category = std::bidirectional_iterator_tag;
 
+  private:
+	Bucket* bucket;
+	size_type index;
+
+  public:
 	friend class BucketStorage;
 
-	AbstractIterator();
-	AbstractIterator(const AbstractIterator &other);
-	~AbstractIterator();
+	AbstractIterator() = default;
+	AbstractIterator(const AbstractIterator& other);
+	~AbstractIterator() = default;
 
-	AbstractIterator &operator=(AbstractIterator&other);
+	AbstractIterator &operator=(const AbstractIterator& other);
 
 	AbstractIterator operator++(int);
 	AbstractIterator &operator++();
@@ -176,27 +270,71 @@ class BucketStorage< T >::AbstractIterator
 
 	reference operator*() const;
 	pointer operator->() const;
+
+  private:
+	AbstractIterator(Bucket* bucket, size_type index);
 };
+
+// ------------------------------------------
+// START OF GENERAL BUCKET CONTENT IMPLEMENTATION
+// ------------------------------------------
+
+template< typename T >
+BucketStorage<T>::GeneralBucketContent::GeneralBucketContent(size_type blockCapacity):
+	blockCapacity(blockCapacity),
+	idCounter(0)
+{
+}
+template< typename T >
+BucketStorage<T>::GeneralBucketContent::GeneralBucketContent(const BucketStorage<T>::GeneralBucketContent& other):
+	blockCapacity(other.blockCapacity),
+	idCounter(other.idCounter)
+{
+}
+template< typename T >
+BucketStorage<T>::size_type BucketStorage<T>::GeneralBucketContent::getBlockCapacity() const noexcept {
+	return blockCapacity;
+}
+template< typename T >
+BucketStorage<T>::id_type BucketStorage<T>::GeneralBucketContent::id() noexcept {
+	return idCounter++;
+}
 
 // ------------------------------------------
 // START OF BUCKET STORAGE IMPLEMENTATION
 // ------------------------------------------
 
 template<typename T>
-BucketStorage<T>::BucketStorage()
+BucketStorage<T>::BucketStorage():
+	generalContent(),
+	dataSize(0),
+	first(nullptr),
+	last(new Bucket()),
+	incomplete(last)
 {
-
 }
 template< typename T >
 BucketStorage<T>::BucketStorage(const BucketStorage< T > &other)
 {
+	// TODO: implementation
 }
 template< typename T >
-BucketStorage<T>::BucketStorage(BucketStorage< T > &&other) noexcept
+BucketStorage<T>::BucketStorage(BucketStorage< T > &&other) noexcept:
+	generalContent(other.generalContent),
+	dataSize(other.dataSize),
+	first(other.first),
+	last(other.last),
+	incomplete(other.last)
 {
+	// TODO: replace other data with valid data
 }
 template< typename T >
-BucketStorage<T>::BucketStorage(BucketStorage::size_type block_capacity)
+BucketStorage<T>::BucketStorage(size_type block_capacity):
+	generalContent(block_capacity),
+	dataSize(0),
+	first(nullptr),
+	last(new Bucket()),
+	incomplete(last)
 {
 }
 template< typename T >
@@ -216,12 +354,26 @@ BucketStorage<T>& BucketStorage< T >::operator=(BucketStorage< T > &&other) noex
 template< typename T >
 BucketStorage<T>::iterator BucketStorage< T >::insert(T &&value)
 {
+	// TODO: implementation
 	return BucketStorage::iterator();
 }
 template< typename T >
 BucketStorage<T>::iterator BucketStorage< T >::insert(const T &value)
 {
-	return BucketStorage::iterator();
+	if (incomplete->isEnd()) {
+		if (last->getPrev() == nullptr)  {
+			incomplete = new Bucket(&generalContent, last);
+		} else {
+			incomplete = new Bucket(&generalContent, last->getPrev(), last);
+		}
+	}
+	auto temp =  incomplete->insert(value);
+	if (incomplete->isFull()) {
+		incomplete = incomplete->getNextIncomplete();
+		incomplete->getPrevIncomplete()->setNextIncomplete(nullptr);
+		incomplete->setPrevIncomplete(nullptr);
+	}
+	return temp;
 }
 template< typename T >
 BucketStorage<T>::iterator BucketStorage< T >::erase(BucketStorage::const_iterator it)
@@ -231,17 +383,17 @@ BucketStorage<T>::iterator BucketStorage< T >::erase(BucketStorage::const_iterat
 template< typename T >
 bool BucketStorage< T >::empty() const noexcept
 {
-	return false;
+	return capacity() == dataSize;
 }
 template< typename T >
 BucketStorage<T>::size_type BucketStorage< T >::size() const noexcept
 {
-	return 0;
+	return dataSize;
 }
 template< typename T >
 BucketStorage<T>::size_type BucketStorage< T >::capacity() const noexcept
 {
-	return 0;
+	return generalContent.getBlockCapacity();
 }
 template< typename T >
 void BucketStorage< T >::shrink_to_fit()
@@ -253,6 +405,10 @@ void BucketStorage< T >::clear()
 }
 template< typename T >
 void BucketStorage<T>::swap(BucketStorage< T > &other) noexcept
+{
+}
+template < typename T >
+void swap(BucketStorage<T>& first, BucketStorage<T>& second)
 {
 }
 template< typename T >
@@ -300,101 +456,327 @@ BucketStorage<T>::const_iterator BucketStorage< T >::cend() const noexcept
 // START OF BUCKET IMPLEMENTATION
 // ------------------------------------------
 
+template< typename T >
+template< typename U >
+U* BucketStorage< T >::Bucket::getMemory(size_type count) const {
+	void* memory = malloc(sizeof(U) * count);
+
+	if (memory != nullptr) {
+		return static_cast<U*>(memory);
+	}
+	// TODO: replace
+	throw 1;
+}
+
+//try
+//{
+//	return static_cast<U*>(::operator new[](count * sizeof(U)));
+//} catch (...) {
+//	// TODO: implementation
+//	throw 1;
+//}
+template< typename T >
+BucketStorage< T >::Bucket::Bucket():
+	generalContent(nullptr),
+	id(std::numeric_limits<std::size_t>::max()),
+	next(nullptr),
+	prev(nullptr),
+	nextIncomplete(nullptr),
+	prevIncomplete(nullptr),
+	data(nullptr),
+	size(0),
+	firstIndex(0),
+	lastIndex(0),
+	nextData(nullptr),
+	prevData(nullptr),
+	idData(nullptr)
+{
+}
+template< typename T >
+BucketStorage< T >::Bucket::Bucket(GeneralBucketContent* generalContent, Bucket* last):
+	generalContent(generalContent),
+	id(generalContent->id()),
+	next(last),
+	prev(nullptr),
+	nextIncomplete(last),
+	prevIncomplete(nullptr),
+	data(getMemory<T>(generalContent->getBlockCapacity())),
+	size(0),
+	firstIndex(0),
+	lastIndex(0),
+	nextData(getMemory<size_type>(generalContent->getBlockCapacity())),
+	prevData(getMemory<size_type>(generalContent->getBlockCapacity())),
+	idData(getMemory<id_type>(generalContent->getBlockCapacity()))
+{
+	last->prev = this;
+	last->prevIncomplete = this;
+}
+template< typename T >
+BucketStorage< T >::Bucket::Bucket(GeneralBucketContent* generalContent, Bucket* prev, Bucket* last):
+	generalContent(generalContent),
+	id(generalContent->id()),
+	next(last),
+	prev(prev),
+	nextIncomplete(last),
+	prevIncomplete(nullptr),
+	data(getMemory<T>(generalContent->getBlockCapacity())),
+	size(0),
+	firstIndex(0),
+	lastIndex(0),
+	nextData(getMemory<size_type>(generalContent->getBlockCapacity())),
+	prevData(getMemory<size_type>(generalContent->getBlockCapacity())),
+	idData(getMemory<id_type>(generalContent->getBlockCapacity()))
+{
+	last->prev = this;
+	last->prevIncomplete = this;
+	prev->next = this;
+}
+template< typename T >
+BucketStorage<T>::Bucket::~Bucket() {
+	delete[] data;
+	delete[] nextData;
+	delete[] prevData;
+	delete[] idData;
+
+	data = nullptr;
+	nextData = nullptr;
+	prevData = nullptr;
+	idData = nullptr;
+}
+template< typename T >
+BucketStorage< T >::size_type BucketStorage< T >::Bucket::beginIndex() const
+{
+
+}
+template< typename T >
+BucketStorage< T >::size_type BucketStorage< T >::Bucket::endIndex() const
+{
+
+}
+template< typename T >
+BucketStorage< T >::size_type BucketStorage< T >::Bucket::nextIndex(size_type index) const
+{
+
+}
+template< typename T >
+BucketStorage< T >::size_type BucketStorage< T >::Bucket::prevIndex(size_type index) const
+{
+
+}
+template< typename T >
+BucketStorage< T >::Bucket* BucketStorage< T >::Bucket::nextBucket() const
+{
+
+}
+template< typename T >
+BucketStorage< T >::Bucket* BucketStorage< T >::Bucket::prevBucket() const
+{
+
+}
+template< typename T >
+bool BucketStorage< T >::Bucket::isEnd() const
+{
+	return next == nullptr;
+}
+template< typename T >
+BucketStorage< T >::Bucket::reference BucketStorage< T >::Bucket::getReference(size_type index)
+{
+	return data[index];
+}
+template< typename T >
+BucketStorage< T >::Bucket::pointer BucketStorage< T >::Bucket::getPointer(size_type index)
+{
+	return &data[index];
+}
+template< typename T >
+BucketStorage< T >::Bucket::const_reference BucketStorage< T >::Bucket::getReference(size_type index) const
+{
+	return data[index];
+}
+template< typename T >
+BucketStorage< T >::Bucket::const_pointer BucketStorage< T >::Bucket::getPointer(size_type index) const
+{
+	return &data[index];
+}
+template< typename T >
+void BucketStorage< T >::Bucket::checkFreeSpace() const
+{
+	if (isFull()) {
+		throw 1; // TODO: replace
+	}
+}
+template< typename T >
+BucketStorage< T >::size_type BucketStorage< T >::Bucket::prepareInsert() {
+	checkFreeSpace();
+
+	size_type index;
+	// TODO: redo
+	if (isEmpty()) {
+		// Заполняем первый элемент
+		index = 0;
+		firstIndex = 0;
+		lastIndex = 0;
+	} else if (nextData[lastIndex] == firstIndex) {
+		// Новый слой
+		index = size;
+	} else {
+		// Дозаполняем слои
+		index = nextData[lastIndex];
+	}
+
+	++size;
+	nextData[index] = firstIndex;
+	prevData[index] = lastIndex;
+	idData[index] = generalContent->id();
+	nextData[lastIndex] = index;
+	prevData[firstIndex] = index;
+	lastIndex = index;
+	return index;
+}
+template< typename T >
+BucketStorage< T >::iterator BucketStorage< T >::Bucket::insert(const T& value)
+{
+	size_type index = prepareInsert();
+	data[index] = value;
+	return iterator(this, index);
+}
+template< typename T >
+BucketStorage< T >::iterator BucketStorage< T >::Bucket::insert(T&& value)
+{
+	size_type index = prepareInsert();
+	data[index] = value;
+	return iterator(this, index);
+}
+template< typename T >
+bool BucketStorage< T >::Bucket::isFull() const
+{
+	return size == generalContent->getBlockCapacity();
+}
+template< typename T >
+bool BucketStorage< T >::Bucket::isEmpty() const
+{
+	return size == 0;
+}
+
 // ------------------------------------------
 // START OF ITERATOR IMPLEMENTATION
 // ------------------------------------------
 
-template<class T>
+template<typename T>
 template<bool isConst>
-BucketStorage< T >::AbstractIterator<isConst>::AbstractIterator()
+BucketStorage< T >::AbstractIterator<isConst>::AbstractIterator(const AbstractIterator &other):
+	bucket(other.bucket),
+	index(other.index)
 {
 }
-template<class T>
+template<typename T>
 template<bool isConst>
-BucketStorage< T >::AbstractIterator<isConst>::AbstractIterator(const AbstractIterator &other)
+BucketStorage< T >::AbstractIterator<isConst> &BucketStorage< T >::AbstractIterator<isConst>::operator=(const AbstractIterator &other)
 {
+	if (*this == other) {
+		return *this;
+	}
+	return *this;
 }
-template<class T>
-template<bool isConst>
-BucketStorage< T >::AbstractIterator<isConst>::~AbstractIterator()
-{
-}
-template<class T>
-template<bool isConst>
-BucketStorage< T >::AbstractIterator<isConst> &BucketStorage< T >::AbstractIterator<isConst>::operator=(AbstractIterator &other)
-{
-	return BucketStorage< T >::AbstractIterator();
-}
-template<class T>
+template<typename T>
 template<bool isConst>
 BucketStorage< T >::AbstractIterator<isConst> BucketStorage< T >::AbstractIterator<isConst>::operator++(int)
 {
+	auto temp = *this;
+	++(*this);
+	return temp;
 }
-template<class T>
+template<typename T>
 template<bool isConst>
 BucketStorage< T >::AbstractIterator<isConst> &BucketStorage< T >::AbstractIterator<isConst>::operator++()
 {
+	if (index != bucket->endIndex()) {
+		index = bucket->nextIndex(index);
+	} else {
+		bucket = bucket->nextBucket();
+		index = bucket->beginIndex();
+	}
 	return *this;
 }
-template<class T>
+template<typename T>
 template<bool isConst>
 BucketStorage< T >::AbstractIterator<isConst> BucketStorage< T >::AbstractIterator<isConst>::operator--(int)
 {
+	auto temp = *this;
+	--(*this);
+	return temp;
 }
-template<class T>
+template<typename T>
 template<bool isConst>
 BucketStorage< T >::AbstractIterator<isConst> &BucketStorage< T >::AbstractIterator<isConst>::operator--()
 {
+	if (index != bucket->beginIndex()) {
+		index = bucket->prevIndex(index);
+	} else {
+		bucket = bucket->prevBucket();
+		index = bucket->endIndex();
+	}
+	return *this;
 }
-template<class T>
+template<typename T>
 template<bool isConst>
 bool BucketStorage< T >::AbstractIterator<isConst>::operator==(const AbstractIterator &other) const
 {
-	return false;
+	return bucket == other.bucket && index == other.index;
 }
-template<class T>
+template<typename T>
 template<bool isConst>
 bool BucketStorage< T >::AbstractIterator<isConst>::operator!=(const AbstractIterator &other) const
 {
-	return false;
+	return !(*this == other);
 }
-template<class T>
+template<typename T>
 template<bool isConst>
 bool BucketStorage< T >::AbstractIterator<isConst>::operator<=(const AbstractIterator &other) const
 {
 	return false;
 }
-template<class T>
+template<typename T>
 template<bool isConst>
 bool BucketStorage< T >::AbstractIterator<isConst>::operator<(const AbstractIterator &other) const
 {
 	return false;
 }
-template<class T>
+template<typename T>
 template<bool isConst>
 bool BucketStorage< T >::AbstractIterator<isConst>::operator>=(const AbstractIterator &other) const
 {
 	return false;
 }
-template<class T>
+template<typename T>
 template<bool isConst>
 bool BucketStorage< T >::AbstractIterator<isConst>::operator>(const AbstractIterator &other) const
 {
 	return false;
 }
-template<class T>
+template<typename T>
 template<bool isConst>
 BucketStorage< T >::AbstractIterator<isConst>::operator AbstractIterator< !isConst >() const
 {
+	return *this;
 }
-template<class T>
+template<typename T>
 template<bool isConst>
 BucketStorage< T >::AbstractIterator<isConst>::reference BucketStorage< T >::AbstractIterator<isConst>::operator*() const
 {
+	return bucket->getReference(index);
 }
-template<class T>
+template<typename T>
 template<bool isConst>
 BucketStorage< T >::AbstractIterator<isConst>::pointer BucketStorage< T >::AbstractIterator<isConst>::operator->() const
+{
+	return bucket->getPointer(index);
+}
+template<typename T>
+template<bool isConst>
+BucketStorage< T >::AbstractIterator<isConst>::AbstractIterator(Bucket* bucket, size_type index):
+	bucket(bucket),
+	index(index)
 {
 }
 
